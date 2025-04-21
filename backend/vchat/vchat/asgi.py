bf0fffa4bdb2ed2bfe679551b69cff9953eb9628
Amraz Rafeeque <amrazrafeek2020@gmail.com>
2025-04-21 22:09:44 +0530

    fix:edit asgi.py

--- backend/vchat/vchat/asgi.py
+++ backend/vchat/vchat/asgi.py
@@ -1,14 +1,18 @@
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from users import routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zchat.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vchat.settings')
+
+# Import routing after setting the Django settings module
+from users import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(routing.websocket_urlpattern)
-    )
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
 })
