456c5c2b8ad7dbcbdfa299da57a7e5684c0d35c8
Amraz Rafeeque <amrazrafeek2020@gmail.com>
2025-04-22 10:19:37 +0530

    fix:image loading issue

--- backend/vchat/vchat/settings.py
+++ backend/vchat/vchat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["vchat-dev.vercel.app", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -164,10 +164,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 AUTH_USER_MODEL = "users.CustomUser"
 
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  "https://vchat-dev.vercel.app"
+    "http://localhost:5173",
 ]
